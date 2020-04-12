308624527ce3173278d30ebc8c410b15d55a56ea
Developer-Dewd-Labs <wildun.dwd@gmail.com>
2020-04-11 20:04:38 -0400

    Init and added blzconfig credentials

---
+++ samples/blzconfig/blz-config.go
@@ -0,0 +1,14 @@
+package blzconfig
+
+import "fmt"
+
+type BlzConfig struct{
+	    address string
+	        mnemonic string
+		    endpoint string
+		        chain_id string
+			    gas_params string
+		    };
+		    var (
+			    blzconfig = BlzConfig{"bluzelle1xhz23a58mku7ch3hx8f9hrx6he6gyujq57y3kp","volcano arrest ceiling physical concert...", "http://localhost:1317", "bluzelle", "{'max_gas': '', 'max_fee': '', 'gas_price': '10'}" }
+		    )
