10fa3586a8a5fab9736ddf663395dc321b6d7d9a
Developer-Dewd-Labs <wildun.dwd@gmail.com>
2020-04-18 12:07:03 -0400

    Initalized constants and variables in COSMOS

--- src/swarmclient/cosmos.go
+++ src/swarmclient/cosmos.go
@@ -2,7 +2,29 @@ package main
 
 import "log"
 
+/*
+Libs to implement or import in GoLang
 
+const util = require('./util');
+const axios = require ('axios');
+const ec = require ('elliptic').ec;
+const bech32 = require ('bech32');
+const bitcoinjs = require('bitcoinjs-lib');
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+
+*/
+var app_endpoint := "http://localhost:1317"
+const tx_command := "txs";
+//const secp256k1 := Use secp256k1-go
+const prefix := 'bluzelle'
+const path := "m/44'/118'/0'/0/0"
+var private_key;
+var account_info := "{ account_number : "", sequence : ""}"
+var tx_queue := []
+const token_name := 'ubnt'
+const MAX_RETRIES := 10
+const RETRY_INTERVAL := 1000 // 1 second
 
 //func advance_queue()
 //func begin_tx()
