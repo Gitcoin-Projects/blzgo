2651baf4ec59142218c125d1deb4bfcaa4cd4f0c
Developer-Dewd-Labs <wildun.dwd@gmail.com>
2020-04-18 12:52:43 -0400

    Added transaction struct and capitalized variable declarations COSMOS

--- src/swarmclient/cosmos.go
+++ src/swarmclient/cosmos.go
@@ -14,33 +14,43 @@ const bip32 = require('bip32');
 const bip39 = require('bip39');
 
 */
-var app_endpoint := "http://localhost:1317"
-const tx_command := "txs";
+var App_endpoint := "http://localhost:1317"
+const Tx_command := "txs";
 //const secp256k1 := Use secp256k1-go
-const prefix := 'bluzelle'
-const path := "m/44'/118'/0'/0/0"
-var private_key;
-var account_info := "{ account_number : "", sequence : ""}"
-var tx_queue := []
-const token_name := 'ubnt'
+const Prefix := 'bluzelle'
+const Path := "m/44'/118'/0'/0/0"
+var Private_key;
+var Account_info := "{ account_number : "", sequence : ""}"
+var Tx_queue := []
+const Token_name := 'ubnt'
 const MAX_RETRIES := 10
 const RETRY_INTERVAL := 1000 // 1 second
 
-//func advance_queue()
-//func begin_tx()
-//func exact_error_from_message()
-//func get_address()
-//func get_ec_private_key()
-//func handle_account_response()
-//func init()
-//func make_random_string()
-//func next_tx()
-//func query()
-//func sanitize_string()
-//func send_transaction()
-//func send_tx()
-//func sign_transaction()
-//func update_account_sequence()
+type Transaction struct {
+  Type string
+  data string
+  deferred string
+  gas_price float64
+  max_gas float64
+  max_fee float64
+  retries_left uint8
+}
+
+//func Advance_queue()
+//func Begin_tx()
+//func Exact_error_from_message()
+//func Get_address()
+//func Get_ec_private_key()
+//func Handle_account_response()
+//func Init()
+//func Make_random_string()
+//func Next_tx()
+//func Query()
+//func Sanitize_string()
+//func Send_transaction()
+//func Send_tx()
+//func Sign_transaction()
+//func Update_account_sequence()
 
 func CosmosInitialized() {
   log.Println("Cosmos initialized!")
